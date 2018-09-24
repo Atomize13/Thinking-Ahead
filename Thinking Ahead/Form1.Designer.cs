6d27cded6341ea468901df7ed36be5b2fcdf6e1f
Charlotte Timms <13timmsc@utcsheffield.org.uk>
2018-09-24 12:55:54 +0100

    Add project files.

---
+++ Thinking Ahead/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Thinking_Ahead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.ButtonDoubleCounter = new System.Windows.Forms.Button();
+            this.Listcounter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnTest.Location = new System.Drawing.Point(614, 12);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = false;
+            this.btnTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ButtonDoubleCounter
+            // 
+            this.ButtonDoubleCounter.Location = new System.Drawing.Point(438, 27);
+            this.ButtonDoubleCounter.Name = "ButtonDoubleCounter";
+            this.ButtonDoubleCounter.Size = new System.Drawing.Size(70, 30);
+            this.ButtonDoubleCounter.TabIndex = 1;
+            this.ButtonDoubleCounter.Text = "Double";
+            this.ButtonDoubleCounter.UseVisualStyleBackColor = true;
+            this.ButtonDoubleCounter.Click += new System.EventHandler(this.ButtonDoubleCounter_Click);
+            // 
+            // Listcounter
+            // 
+            this.Listcounter.Location = new System.Drawing.Point(568, 87);
+            this.Listcounter.Name = "Listcounter";
+            this.Listcounter.Size = new System.Drawing.Size(98, 48);
+            this.Listcounter.TabIndex = 2;
+            this.Listcounter.Text = "List button";
+            this.Listcounter.UseVisualStyleBackColor = true;
+            this.Listcounter.Click += new System.EventHandler(this.Listcounter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 549);
+            this.Controls.Add(this.Listcounter);
+            this.Controls.Add(this.ButtonDoubleCounter);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button ButtonDoubleCounter;
+        private System.Windows.Forms.Button Listcounter;
+    }
+}
+
---
+++ Thinking Ahead/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thinking_Ahead
+{
+    public partial class Form1 : Form
+    {
+        //declare variables here
+        public int buttonCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonCounter++; //imcrements it by one
+            MessageBox.Show("Hello World");
+            MessageBox.Show( buttonCounter.ToString());
+        }
+        private void DoubleButtonCounter()
+        {
+            buttonCounter = buttonCounter * 2;
+        }
+
+        private void ButtonDoubleCounter_Click(object sender, EventArgs e)
+        {
+            DoubleButtonCounter();
+            MessageBox.Show(buttonCounter.ToString());
+        }
+        public int findMax(int[] listInt) //all usable function that will find the maxu=imum of a list of given integers called listInt
+        {
+            int highest = 0;
+            for(int i=0;i<listInt.Length;i++) //for integer
+            {
+                if (highest < listInt[i])
+                {
+                    highest = listInt[i];
+                }
+            }
+
+            return highest; //returns chosen value
+        }
+
+        private void Listcounter_Click(object sender, EventArgs e)
+        {
+            int[] num = new int[] { 78, 34, 56, 11, 13, 2002 };
+            int high = findMax(num);
+            MessageBox.Show(high.ToString());
+        }
+    }
+}
